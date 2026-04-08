94432690a27d80c9fe8d9872c82a247e109a66a8
jefra <jefrayle972@gmail.com>
2026-04-08 18:00:42 -0500

    1.0

--- sesion_1.dart
+++ sesion_1.dart
@@ -2,4 +2,18 @@ void main(){
   String? nombre = "Jonathan";
   print("Hola $nombre bienvenido a Dart");
   
+  List<int> numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  numeros.forEach((numero) {
+    print(numero);
+  });
+  Map<String, int> edades = {
+    "John": 30,
+    "Jane": 25,
+    "Judith": 27,
+    "Jeronimo": 29,
+    "Jimy": 31
+  };
+  edades.forEach((nombre, edad) {
+    print("$nombre tiene $edad años");
+  });
 }
